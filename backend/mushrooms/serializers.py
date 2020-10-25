8c52586f8c3acf0a922e61cd8adbca721fc5c066
Mateusz Brodziak <Mateusz@Mateusz>
2020-10-25 15:16:17 +0100

    Add new fields to models and add new informations about mushrooms

--- backend/mushrooms/serializers.py
+++ backend/mushrooms/serializers.py
@@ -5,4 +5,8 @@ from .models import Mushroom
 class MushroomSerializers(serializers.ModelSerializer):
     class Meta:
         model = Mushroom
-        fields = ('id', 'name', 'latin_name', 'application', 'photo')
+        fields = ('id', 'name', 'latin_name', 'application', 'introduction', 'type', 'rim_hat', 'color_hat',
+                  'shape_hat', 'area_hat', 'description_fruitcake', 'milk', 'color_pulp', 'description_pulp',
+                  'details_type', 'type_shaft', 'color_shaft', 'shape_shaft', 'base_shaft', 'area_shaft',
+                  'section_shaft', 'description_shaft', 'taste', 'smell', 'incidence_time', 'season', 'existence',
+                  'habitat', 'photo')
