f17ea9756ecc016ec4c3473c0a6b80189f88a475
Szymon Słonka <szymslo@laptop>
2020-10-28 13:18:29 +0100

    v2.2

--- frontend/src/pages/atlas/Info.jsx
+++ frontend/src/pages/atlas/Info.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { useParams, useLocation } from 'react-router-dom'
 import { useQuery } from "react-query";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from '@material-ui/core/Typography';
 
 const fetchDetails = async (id,loc) => {
     if(loc.pathname.includes("borowiki")) {
@@ -22,7 +28,26 @@ const fetchDetails = async (id,loc) => {
     else return null;
 }
 
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: "white",
+      background: "rgba(41, 40, 38, 0.5)",
+      margin: "0 auto",
+      width: "80%",
+      height: "80%"
+    },
+    image: {
+      opacity: "0.7",
+      height: '100%',
+      width: '100%',
+      borderRadius: '3px'
+    }
+  }));
+
 const Info = () => {
+    const classes = useStyles();
 
     let {id} = useParams();
     let location = useLocation();
@@ -34,8 +59,22 @@ const Info = () => {
     return (
         <div>
             {status === "error" && <p>Błąd pobierania danych z bazy!</p>}
-            {status === "loading" && <p>Wczytywanie danych...</p>}
-            {status === "success" && <h1>{data[0].introduction}</h1>}
+            {status === "success" && (
+                <Box m={5}>
+                    <Container maxWidth="md">
+                        <Card className={classes.paper}>
+                            <CardMedia className={classes.image}
+                                component="img"
+                                alt={data[0].name}
+                                image={'http://127.0.0.1:8000' + (data[0].photo)}
+                                opacity="0.5"
+                                title={data[0].name}
+                            />  
+                        </Card> 
+                    </Container>
+                </Box>
+            )}
+            
         </div>    
         )
 }
