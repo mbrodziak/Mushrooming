bc93f2655c0e4b7e8e7f5fcf12c5da937bd284d1
Mateusz Brodziak <Mateusz@Mateusz>
2020-10-30 23:26:40 +0100

    Delete redundant path

--- backend/mushrooms/urls.py
+++ backend/mushrooms/urls.py
@@ -10,7 +10,4 @@ router.register('mushrooms', MushroomViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    # path('api/mushroom/', views.MushroomList.as_view()),
-    # path('api/mushroom/<str:category>/', views.get_mushroom_category),
-    # path('api/mushroom/<str:category>/<int:id>', views.get_mushroom_category_object),
 ]
